92d078f5c9cc0f27c100f00662bd272c0a7f5890
saifmohsen105 <168905609+saifmohsen105@users.noreply.github.com>
2025-03-23 16:28:42 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,12 @@ export const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
   {
     path: 'sign-in',
-    loadComponent: () => import('./sign-in/sign-in.component').then(m => m.SignInComponent)
+    loadComponent: () => import('./sign-in/sign-in.component').then(m => m.SignInComponent) , title:"sign-in"
   },
   {
     path: 'sign-up',
-    loadComponent: () => import('./sign-up/sign-up.component').then(m => m.SignUpComponent)
+    loadComponent: () => import('./sign-up/sign-up.component').then(m => m.SignUpComponent), title:"sign-up"
   },
-  { path: 'chat', component: ChatComponent },
+  { path: 'chat', component: ChatComponent , title:"chat"},
   { path: '**', component: NotFoundComponent },
 ];
